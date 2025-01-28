87c61f1340908b8251aa59364bf64ec336f9d641
Tom DA COSTA <146847308+tom-da-costa@users.noreply.github.com>
2025-01-28 13:44:43 +0100

    chore : apply ruff lint and format (#8)

--- .save/snippets/bat.py
+++ .save/snippets/bat.py
@@ -3,7 +3,7 @@ import serial
 
 ser = serial.Serial("/dev/ttyUSB0", baudrate=1200)
 
-ser.write(b'~20014682C0048520FCC3\r')
+ser.write(b"~20014682C0048520FCC3\r")
 
 time.sleep(1)
 
@@ -14,11 +14,11 @@ time.sleep(0.1)
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
 
 # ser.write(b'pwr\n')
-ser.write(b'bat\n')
+ser.write(b"bat\n")
 
 resp = ""
-while(ser.in_waiting != 0):
-  resp += ser.read().decode()
+while ser.in_waiting != 0:
+    resp += ser.read().decode()
 print(resp)
 
 ser.close()
--- .save/snippets/wakeup_us2000b.py
+++ .save/snippets/wakeup_us2000b.py
@@ -3,13 +3,12 @@ import serial
 import sys, getopt
 
 
-
 cmd = " ".join(sys.argv[1:])
 print(cmd)
 
 ser = serial.Serial("/dev/ttyUSB0", baudrate=1200)
 
-ser.write(b'~20014682C0048520FCC3\r')
+ser.write(b"~20014682C0048520FCC3\r")
 
 time.sleep(1)
 
@@ -20,13 +19,15 @@ time.sleep(0.1)
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
 
 # ser.write(b'pwr\n')
-ser.write(bytes(cmd + "\n", 'ascii'))
+ser.write(bytes(cmd + "\n", "ascii"))
+
 
 def readAndPrint():
-  resp = ""
-  while(ser.in_waiting != 0):
-    resp += ser.read().decode()
-  print(resp)
+    resp = ""
+    while ser.in_waiting != 0:
+        resp += ser.read().decode()
+    print(resp)
+
 
 readAndPrint()
 
